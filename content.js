ca3d342f78702b2815779916a88c47c4f5f4563b
Tom Casavant <tfcasavant@gmail.com>
2024-10-23 16:09:43 -0400

    Added date customization, word drop options, adjusted div styling a bit

--- content.js
+++ content.js
@@ -1,16 +1,30 @@
 // Extract the search term from DuckDuckGo's URL
 const urlParams = new URLSearchParams(window.location.search);
 const searchTerm = urlParams.get('q');
-
+console.log("Search Term: ", searchTerm);
 if (searchTerm) {
     // Request the settings from the background script
     browser.runtime.sendMessage({ action: 'getSettings' })
         .then(settings => {
+            console.log("Settings: ", settings);
             const mastodonDomain = settings.domain;
             const numPosts = settings.numPosts;
+            const dropWords = settings.dropWords;
+            const dateType = settings.dateType;
 
+            console.log("All settings: ", settings);
+            console.log("Mastodon Domain: ", mastodonDomain);
+            console.log("Number of Posts: ", numPosts);
+            console.log("Drop Words: ", dropWords);
+            console.log("Date Type: ", dateType);
 
-            browser.runtime.sendMessage({ action: 'searchMastodon', searchTerm: searchTerm })
+            // Parse dropWords as a list, then drop words from the search term that match any of the words in the list
+            const dropWordsList = dropWords.split(',').map(word => word.trim());
+
+            // Using Regex instead of a simple split in case someone wants to include the space or have phrases dropped
+            const replacedSearchTerm = searchTerm.replace(new RegExp(dropWordsList.join('|'), 'gi'), '');
+            console.log("Replaced Search Term: ", replacedSearchTerm);
+            browser.runtime.sendMessage({ action: 'searchMastodon', searchTerm: replacedSearchTerm })
                 .then(response => {
 					console.log(response);
                     if (response.success) {
@@ -24,10 +38,17 @@ if (searchTerm) {
                             var postDiv = document.createElement('div');
                             postDiv.className = 'duckducksocial-post'; 
 
+                            var usernameWrapper = document.createElement('div');
+                            usernameWrapper.className = 'duckducksocial-username-wrapper';
+
                             var username = document.createElement('p');
                             username.textContent = `@${post.account.acct}`;
-                            username.className = 'duckducksocial-username'; 
-
+                            username.className = 'duckducksocial-username';
+                            // Add <hr> separator between username and content
+                            var hr = document.createElement('hr');
+                            hr.className = 'duckducksocial-hr';
+                            usernameWrapper.appendChild(username);
+                            usernameWrapper.appendChild(hr);
 
 							var sanitizedContent = DOMPurify.sanitize(post.content);
                             var content = document.createElement('p');
@@ -37,14 +58,40 @@ if (searchTerm) {
 
                             // Datetime
                             var datetime = document.createElement('p');
-                            datetime.textContent = new Date(post.created_at).toLocaleString();
+                            // If the dateType is set to 'relative', use a relative date otherwise use an absolute date
+                            if (dateType === 'relative') {
+                                // Calculate time since post was created
+                                var now = new Date();
+                                var created = new Date(post.created_at);
+                                var diff = now - created;
+                                var seconds = Math.floor(diff / 1000);
+                                var minutes = Math.floor(seconds / 60);
+                                var hours = Math.floor(minutes / 60);
+                                var days = Math.floor(hours / 24);
+                                var months = Math.floor(days / 30);
+                                var years = Math.floor(months / 12);
+                                // Adjust how date is displayed depending on how long ago the post was created
+                                if (years > 0) {
+                                    datetime.textContent = years + 'y';
+                                } else if (days > 0) {
+                                    datetime.textContent = days + 'd';
+                                } else if (hours > 0) {
+                                    datetime.textContent = hours + 'h';
+                                } else if (minutes > 0) {
+                                    datetime.textContent = minutes + 'm';
+                                } else {
+                                    datetime.textContent = seconds + 's';
+                                }
+                            } else {
+                                datetime.textContent = new Date(post.created_at).toLocaleString();
+                            }
                             datetime.className = 'duckducksocial-datetime';
 
                             var postHref = document.createElement('a');
                             postHref.href = `https://${mastodonDomain}/@${post.account.acct}/${post.id}`;
                             postHref.appendChild(postDiv);
 
-                            postDiv.appendChild(username);
+                            postDiv.appendChild(usernameWrapper);
                             postDiv.appendChild(content);
 							if (post.media_attachments && post.media_attachments.length > 0) {
 								var mediaContainer = document.createElement('div');
